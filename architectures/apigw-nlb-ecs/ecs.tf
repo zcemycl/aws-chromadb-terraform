cfc891a672393c4cbf9cb97c97d6eed428b6a381
zcemycl <lyc010197@gmail.com>
2023-09-01 10:26:31 +0100

    update a4

---
+++ architectures/apigw-nlb-ecs/ecs.tf
@@ -0,0 +1,90 @@
+resource "aws_iam_role" "chroma_ecs_task_role" {
+  name               = "chroma_ecs_task_role"
+  assume_role_policy = file("policies/ecs-task-role.json")
+}
+
+resource "aws_iam_policy" "chroma_ecs_autoscaling_policy" {
+  name        = "chroma-autoscaling-policy"
+  path        = "/"
+  description = "Policy for triggering autoscaling policies"
+  policy      = file("policies/ecs-autoscale-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "chroma_ecs_autoscaling_policy_attachment" {
+  role       = aws_iam_role.chroma_ecs_task_role.name
+  policy_arn = aws_iam_policy.chroma_ecs_autoscaling_policy.arn
+}
+
+resource "aws_iam_role" "chroma_ecs_task_execution_role" {
+  name               = "chroma-ecsTaskExecutionRole"
+  assume_role_policy = file("policies/ecs-task-role.json")
+}
+
+resource "aws_iam_role_policy_attachment" "chroma_ecs_task_execution_role_policy_attachment" {
+  role       = aws_iam_role.chroma_ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_cluster" "chroma_cluster" {
+  name = "chroma-cluster"
+}
+
+resource "aws_ecs_task_definition" "chroma_task_definition" {
+  family                   = "chroma-task-definition"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 512
+  memory                   = 2048
+  execution_role_arn       = aws_iam_role.chroma_ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.chroma_ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name        = "chroma-container"
+      image       = "ghcr.io/chroma-core/chroma:0.4.7"
+      essential   = true
+      environment = []
+      portMappings = [
+        {
+          containerPort = 8000
+          hostPort      = 8000
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = module.loggings.chroma_log_group
+          awslogs-stream-prefix = "chroma"
+          awslogs-region        = var.aws_region
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_service" "chroma_ecs_service" {
+  name                               = "chroma-service"
+  cluster                            = aws_ecs_cluster.chroma_cluster.id
+  task_definition                    = aws_ecs_task_definition.chroma_task_definition.arn
+  desired_count                      = 1
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+  launch_type                        = "FARGATE"
+  scheduling_strategy                = "REPLICA"
+
+  network_configuration {
+    security_groups = [module.security_groups.chroma_sg_id]
+    subnets         = module.chroma_network.subnet_ids
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.lbtg.arn
+    container_name   = "chroma-container"
+    container_port   = 8000
+  }
+
+  depends_on = [
+    aws_lb_listener.lbl,
+    aws_lb_target_group.lbtg
+  ]
+}
--- modules/security_groups/outs.tf
+++ modules/security_groups/outs.tf
@@ -1,7 +1,3 @@
-output "chroma_instance_sg_id" {
-  value = aws_security_group.sg["ec2-chroma"].id
-}
-
-output "backdoor_instance_sg_id" {
-  value = aws_security_group.sg["ec2-backdoor"].id
+output "chroma_sg_id" {
+  value = aws_security_group.sg["chroma"].id
 }
